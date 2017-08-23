765379651c0e94cb23bf4a03c965fc1b0288c12a
George Fourtounis <gfour@di.uoa.gr>
2017-08-23 23:36:35 +0300

    Add debugging statements, use portable HOME dir, don't ignore exceptions, fix agent package name.

--- ctxagent/src/main/java/heapdl/ctxenhancer/Agent/Transformer.java
+++ ctxagent/src/main/java/heapdl/ctxenhancer/Agent/Transformer.java
@@ -11,18 +11,27 @@ import java.util.Arrays;
 
 public class Transformer implements ClassFileTransformer {
 
+    private static boolean debug = true;
+    private static String homeDir;
     private boolean optInstrumentCGE = true;
     public Transformer(boolean optInstrumentCGE) {
         this.optInstrumentCGE = optInstrumentCGE;
     }
 
     public static synchronized void premain(String args, Instrumentation inst) throws ClassNotFoundException, IOException, NotFoundException {
+        this.homeDir = System.getenv("HOME");
+        if (homeDir == null) {
+            System.err.println("Cannot find HOME environment variable, aborting.");
+            System.exit(-1);
+        } else {
+            debugMessage("Found HOME = " + homeDir);
+        }
+
         ClassPool cp = ClassPool.getDefault();
-        cp.insertClassPath("/home/neville/doop-benchmarks/dacapo-bach/avrora.jar");
-        cp.insertClassPath("/home/neville/doop-benchmarks/dacapo-bach/avrora-deps.jar");
-        inst.addTransformer(new Transformer(
-                args.contains("cg")
-        ));
+        cp.insertClassPath(homeDir + "/doop-benchmarks/dacapo-bach/avrora.jar");
+        cp.insertClassPath(homeDir + "/doop-benchmarks/dacapo-bach/avrora-deps.jar");
+        boolean optCGE = (args != null) && args.contains("cg");
+        inst.addTransformer(new Transformer(optCGE));
 
     }
 
@@ -31,12 +40,18 @@ public class Transformer implements ClassFileTransformer {
     }
 
     private static boolean isInterestingClass(String name) {
-        if (name.startsWith("javassist"))
+        String nameDots = name.replace("/", ".");
+        if (nameDots.startsWith("javassist"))
             return false;
-        if (name.startsWith("Instrumentation"))
+        if (nameDots.startsWith("Instrumentation"))
             return false;
-        if (name.equals("java/lang/String"))
+        if (nameDots.startsWith("heapdl.ctxenhancer"))
             return false;
+        if (nameDots.equals("java.lang.String"))
+            return false;
+        // if (nameDots.equals("java.lang.StringBuilder"))
+        //     return false;
+        debugMessage("interesting class: " + nameDots);
         return true;
     }
 
@@ -44,10 +59,11 @@ public class Transformer implements ClassFileTransformer {
                             ProtectionDomain pd, byte[] classFile) throws IllegalClassFormatException {
         CtClass cls = null;
         if (isLibraryClass(className)) return null;
+        debugMessage("Transforming: " + className);
         try {
             ClassPool cp = ClassPool.getDefault();
-            cp.appendClassPath("/home/neville/doop-benchmarks/dacapo-bach/avrora.jar");
-            cp.appendClassPath("/home/neville/doop-benchmarks/dacapo-bach/avrora-deps.jar");
+            cp.appendClassPath(homeDir + "/doop-benchmarks/dacapo-bach/avrora.jar");
+            cp.appendClassPath(homeDir + "/doop-benchmarks/dacapo-bach/avrora-deps.jar");
             //cp.insertClassPath(new ByteArrayClassPath(className.replace("/","."), classFile));
             cls = cp.get(className.replace("/","."));
 
@@ -64,7 +80,8 @@ public class Transformer implements ClassFileTransformer {
                 if (Modifier.isNative(m.getModifiers()))
                     return;
                 if (!Modifier.isStatic(m.getModifiers()) && !Modifier.isAbstract(m.getModifiers()) && optInstrumentCGE) {
-                    m.insertBefore("Instrumentation.Recorder.Recorder.recordCall($0);");
+                    debugMessage("Instrumentation#1");
+                    m.insertBefore("heapdl.ctxenhancer.Recorder.Recorder.recordCall($0);");
                 }
 
                 m.instrument(new ExprEditor() {
@@ -72,17 +89,21 @@ public class Transformer implements ClassFileTransformer {
                         if (!isInterestingClass(newExpr.getClassName()))
                            return;
                         if (Modifier.isStatic(m.getModifiers())) {
-                            newExpr.replace("{ $_ = $proceed($$);   Instrumentation.Recorder.Recorder.recordStatic($_); }");
+                            debugMessage("Instrumentation#2");
+                            newExpr.replace("{ $_ = $proceed($$);   heapdl.ctxenhancer.Recorder.Recorder.recordStatic($_); }");
                         } else {
-                            newExpr.replace("{ $_ = $proceed($$);   Instrumentation.Recorder.Recorder.record(this, $_); }");
+                            debugMessage("Instrumentation#3");
+                            newExpr.replace("{ $_ = $proceed($$);   heapdl.ctxenhancer.Recorder.Recorder.record(this, $_); }");
                         }
                     }
 
                     public void edit(MethodCall call) throws CannotCompileException {
                         if (Modifier.isStatic(m.getModifiers())) {
-                            call.replace(" { Instrumentation.Recorder.Recorder.mergeStatic(); $_ = $proceed($$); }");
+                            debugMessage("Instrumentation#4");
+                            call.replace(" { heapdl.ctxenhancer.Recorder.Recorder.mergeStatic(); $_ = $proceed($$); }");
                         } else {
-                            call.replace(" { Instrumentation.Recorder.Recorder.merge(this); $_ = $proceed($$); }");
+                            debugMessage("Instrumentation#5");
+                            call.replace(" { heapdl.ctxenhancer.Recorder.Recorder.merge(this); $_ = $proceed($$); }");
                         }
                     }
 
@@ -94,10 +115,11 @@ public class Transformer implements ClassFileTransformer {
                             return;
                         }
                         if (Modifier.isStatic(m.getModifiers())) {
-                            newArray.replace("{ $_ = $proceed($$);   Instrumentation.Recorder.Recorder.recordStatic($_); }");
-
+                            debugMessage("Instrumentation#6");
+                            newArray.replace("{ $_ = $proceed($$);   heapdl.ctxenhancer.Recorder.Recorder.recordStatic($_); }");
                         } else {
-                            newArray.replace("{ $_ = $proceed($$);   Instrumentation.Recorder.Recorder.record(this, $_); }");
+                            debugMessage("Instrumentation#7");
+                            newArray.replace("{ $_ = $proceed($$);   heapdl.ctxenhancer.Recorder.Recorder.record(this, $_); }");
                         }
                     }
                 });
@@ -105,7 +127,7 @@ public class Transformer implements ClassFileTransformer {
 
             } catch (Exception e) {
                 // fail silently
-                //e.printStackTrace();
+                e.printStackTrace();
             }
         });
         try {
@@ -113,12 +135,17 @@ public class Transformer implements ClassFileTransformer {
             //System.out.println(cls.getName());
             return cls.toBytecode();
         } catch (Exception e) {
-            //e.printStackTrace();
+            e.printStackTrace();
         } finally {
             cls.detach();
         }
         return null;
 
+    }
+
+    private static void debugMessage(String msg) {
+        if (debug)
+            System.err.println("Context-Agent: " + msg);
     }
 
     private static CtClass getCtClass(String className) throws NotFoundException {
