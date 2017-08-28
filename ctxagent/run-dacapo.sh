d6cc38f2fd70a266b8f9d25c9d5e20689a48f0e8
George Fourtounis <gfour@di.uoa.gr>
2017-08-28 10:50:48 +0300

    Update list of dacapo benchmarks.

--- ctxagent/run-dacapo.sh
+++ ctxagent/run-dacapo.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# Runs the dacapo-bach benchmarks with the context agent. Notes:
+#
+# - tradebeans needs a specific free local port to work
+#
+# - eclipse needs a special agent to run under modern JVMs
+#   (https://github.com/jon-bell/dacapo-eclipse-hacker)
+#
+
 JAVA=java
 
 AGENT=${HOME}/doop-nexgen/HeapDL/ctxagent/build/libs/ctxagent-1.0-SNAPSHOT.jar
 
-for b in avrora batik h2 luindex lusearch sunflow
-# for b in avrora batik eclipse h2 jython luindex lusearch pmd sunflow tradebeans xalan
+for b in avrora batik h2 jython luindex lusearch pmd sunflow xalan
+# for b in eclipse tradebeans
 do
     time ${JAVA} -javaagent:${AGENT}=cg,${b} -Xss10000m -cp ${AGENT}:${HOME}/doop-benchmarks/dacapo-bach/dacapo-9.12-bach.jar Harness ${b} -s default -t 1
 done
