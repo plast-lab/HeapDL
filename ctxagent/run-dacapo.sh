2f3bf5ddb9cf1f450ef5313c48607aa5872b64bf
George Fourtounis <gfour@di.uoa.gr>
2017-08-28 12:10:16 +0300

    Make cg parameter easier to change in run-dacapo.sh.

--- ctxagent/run-dacapo.sh
+++ ctxagent/run-dacapo.sh
@@ -11,9 +11,12 @@
 JAVA=java
 
 AGENT=${HOME}/doop-nexgen/HeapDL/ctxagent/build/libs/ctxagent-1.0-SNAPSHOT.jar
+# CG=cg
+CG=
 
 for b in avrora batik h2 jython luindex lusearch pmd sunflow xalan
 # for b in eclipse tradebeans
 do
-    time ${JAVA} -javaagent:${AGENT}=cg,${b} -Xss10000m -cp ${AGENT}:${HOME}/doop-benchmarks/dacapo-bach/dacapo-9.12-bach.jar Harness ${b} -s default -t 1
+    time ${JAVA} -javaagent:${AGENT}=${CG},${b} -Xss10000m -cp ${AGENT}:${HOME}/doop-benchmarks/dacapo-bach/dacapo-9.12-bach.jar Harness ${b} -s default -t 1
+    exit
 done
