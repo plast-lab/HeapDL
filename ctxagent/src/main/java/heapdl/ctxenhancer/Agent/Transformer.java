aaf7b9f5980930fd4c48e1c5e16429527007f424
George Fourtounis <gfour@di.uoa.gr>
2017-09-13 13:53:38 +0300

    Use ClassNode to avoid 'optimizations for "mostly add" bytecode transformations' so that merge() inside <init>() works.

--- ctxagent/src/main/java/heapdl/ctxenhancer/Agent/Transformer.java
+++ ctxagent/src/main/java/heapdl/ctxenhancer/Agent/Transformer.java
@@ -6,6 +6,7 @@ import java.security.ProtectionDomain;
 import java.util.Arrays;
 
 import org.objectweb.asm.*;
+import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.util.CheckClassAdapter;
 
 public class Transformer implements ClassFileTransformer {
@@ -52,10 +53,12 @@ public class Transformer implements ClassFileTransformer {
 
         ClassReader reader = new ClassReader(classFile);
         int wFlags = ClassWriter.COMPUTE_MAXS; // | ClassWriter.COMPUTE_FRAMES;
-        ClassWriter writer = new ContextClassWriter(loader, reader, wFlags);
+        ClassWriter writer = new ContextClassWriter(loader, wFlags);
         ClassVisitor ctxAdapter = new CtxEnhancherAdapter(writer, className, optInstrumentCGE, loader);
         try {
-            reader.accept(ctxAdapter, ClassReader.EXPAND_FRAMES);
+            ClassNode cn = new ClassNode(Opcodes.ASM5);
+            reader.accept(cn, ClassReader.EXPAND_FRAMES);
+            cn.accept(writer);
         } catch (RuntimeException ex) {
             System.err.println(CTXT_AGENT + "ASM error visiting class " + className);
             ex.printStackTrace();
@@ -67,22 +70,28 @@ public class Transformer implements ClassFileTransformer {
         // Save transformed bytecode.
         if (saveBytecode)
             debugWriteClass(className, ret);
-
-        if (debug) {
-            // Check bytecode using ASM's CheckClassAdapter.
-            ClassReader debugReader = new ClassReader(ret);
-            ClassWriter debugWriter = new ContextClassWriter(loader, reader, wFlags);
-            try {
-                debugReader.accept(new CheckClassAdapter(debugWriter), 0);
-            } catch (RuntimeException ex) {
-                System.err.println("Bytecode check failed for " + className + ":");
-                ex.printStackTrace();
-                System.exit(-1);
-            }
-
-        }
+        // Check bytecode for correctness.
+        if (debug)
+            checkBytecode(loader, className, ret, wFlags);
 
         return ret;
+    }
+
+    // Check bytecode using ASM's CheckClassAdapter.
+    private void checkBytecode(ClassLoader loader, String className,
+                               byte[] bc, int wFlags) {
+        ClassReader debugReader = new ClassReader(bc);
+        ClassWriter debugWriter = new ContextClassWriter(loader, wFlags);
+        try {
+            ClassNode debugNode = new ClassNode(Opcodes.ASM5);
+            debugReader.accept(debugNode, ClassReader.EXPAND_FRAMES);
+            debugNode.accept(new CheckClassAdapter(debugWriter));
+            // debugReader.accept(new CheckClassAdapter(debugWriter), 0);
+        } catch (RuntimeException ex) {
+            System.err.println("Bytecode check failed for " + className + ":");
+            ex.printStackTrace();
+            System.exit(-1);
+        }
     }
 
     private void debugWriteClass(String className, byte[] bytecode) {
