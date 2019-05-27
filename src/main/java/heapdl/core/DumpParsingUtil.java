697db0deec6deb7ae0bf21e5f95c9b08a9235f25
tsampikos <tsampikos92@gmail.com>
2019-05-27 16:08:26 +0300

    add raised types (from anantoni)

--- src/main/java/heapdl/core/DumpParsingUtil.java
+++ src/main/java/heapdl/core/DumpParsingUtil.java
@@ -3,12 +3,34 @@ package heapdl.core;
 import heapdl.hprof.StackFrame;
 
 import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by neville on 27/01/2017.
  */
 public class DumpParsingUtil {
     public static final int UNKNOWN_LINE = 0;
+    private static final Map<String, String> cachedRaisedTypes = new ConcurrentHashMap<>();
+
+    private static final String BOOLEAN = "boolean";
+    private static final String BOOLEAN_JVM = "Z";
+    private static final String INT = "int";
+    private static final String INT_JVM = "I";
+    private static final String LONG = "long";
+    private static final String LONG_JVM = "J";
+    private static final String DOUBLE = "double";
+    private static final String DOUBLE_JVM = "D";
+    private static final String VOID = "void";
+    private static final String VOID_JVM = "V";
+    private static final String FLOAT = "float";
+    private static final String FLOAT_JVM = "F";
+    private static final String CHAR = "char";
+    private static final String CHAR_JVM = "C";
+    private static final String SHORT = "short";
+    private static final String SHORT_JVM = "S";
+    private static final String BYTE = "byte";
+    private static final String BYTE_JVM = "B";
 
     public static int parseLineNumber(String lineNumber) {
         try {
@@ -81,6 +103,7 @@ public class DumpParsingUtil {
                 String[] temp = convertType(compact.substring(1));
                 return new String[]{temp[0] + "[]", temp[1]};
             } else if (compact.startsWith("(")) {
+//                System.out.println(compact);
                 String[] temp = compact.substring(1).split("\\)");
                 if (temp.length != 2)
                     throw new RuntimeException("Unknown: " + compact);
@@ -127,7 +150,100 @@ public class DumpParsingUtil {
     }
 
     public static String methodSignatureFromStackFrame(StackFrame frame) {
-        return convertType(frame.getMethodSignature())[0].replace("<MethodName>", frame.getMethodName());
+        return frame.getMethodSignature().replace("<MethodName>", frame.getMethodName());
     }
 
+
+    public static String raiseTypeId(String id) {
+        String cached = cachedRaisedTypes.get(id);
+        if (cached != null)
+            return cached;
+
+        int typePrefixEndIdx = 0;
+        // Peel off array brackets.
+        while (id.charAt(typePrefixEndIdx) == '[')
+            typePrefixEndIdx++;
+
+        StringBuilder sb;
+        if ((id.charAt(typePrefixEndIdx) == 'L') && (id.charAt(id.length() -1) == ';'))
+            sb = new StringBuilder(id.substring(typePrefixEndIdx + 1, id.length() - 1).replace('/', '.'));
+        else
+            sb = new StringBuilder(decodePrimType(id.substring(typePrefixEndIdx)));
+
+        if (typePrefixEndIdx != 0) {
+            for (int i = 0; i < typePrefixEndIdx; i++)
+                sb.append("[]");
+        }
+
+        String ret = sb.toString();
+
+        // Find multidimensional arrays in bytecode (e.g. '[[C' / 'char[][]') .
+//        if (typePrefixEndIdx > 1) {
+//            System.err.println("Warning: found multidimensional array type: " + id + " -> " + ret);
+//        }
+
+        cachedRaisedTypes.put(id, ret);
+        return ret;
+    }
+    private static String decodePrimType(String id) {
+        switch (id) {
+            case BOOLEAN_JVM : return BOOLEAN;
+            case INT_JVM     : return INT;
+            case LONG_JVM    : return LONG;
+            case DOUBLE_JVM  : return DOUBLE;
+            case VOID_JVM    : return VOID;
+            case FLOAT_JVM   : return FLOAT;
+            case CHAR_JVM    : return CHAR;
+            case SHORT_JVM   : return SHORT;
+            case BYTE_JVM    : return BYTE;
+            default          : throw new RuntimeException("Invalid type id format: " + id);
+        }
+    }
+
+    public static String convertArguments(String arguments) {
+    	//System.out.println("Unraised arguments: " + arguments);
+    	String raisedArguments = "";
+	    String arrayPostfix = "";
+    	int i = 0;
+    	while (i < arguments.length()) {
+		    if (arguments.charAt(i) == 'L') {
+			    String lowLevelType = arguments.substring(i, arguments.indexOf(';', i) + 1);
+			    //System.out.println("Low level type: " + lowLevelType);
+			    if (arguments.indexOf(';', i) != arguments.length() - 1) {
+				    raisedArguments += raiseTypeId(lowLevelType) + arrayPostfix + ",";
+			    }
+			    else {
+				    raisedArguments += raiseTypeId(lowLevelType) + arrayPostfix;
+			    }
+			    arrayPostfix = "";
+			    i = arguments.indexOf(';', i) + 1;
+			}
+		    else if (arguments.charAt(i) == '[') {
+		    	arrayPostfix += "[]";
+		    	i++;
+		    }
+		    else {
+		    	String lowLevelPrimType = "" + arguments.charAt(i);
+		    	if (i != arguments.length() -1) {
+				    raisedArguments += raiseTypeId(lowLevelPrimType) + arrayPostfix + ",";
+			    }
+		    	else {
+				    raisedArguments += raiseTypeId(lowLevelPrimType) + arrayPostfix;
+			    }
+		    	i++;
+		    }
+	    }
+    	//System.out.println("Raised arguments: " + raisedArguments);
+    	return raisedArguments;
+    }
+
+	public static boolean isPrimitiveType(String s) {
+		return (s.equals(BOOLEAN) || s.equals(INT) || s.equals(LONG) ||
+				s.equals(DOUBLE) || s.equals(VOID) || s.equals(FLOAT) ||
+				s.equals(CHAR) || s.equals(SHORT) || s.equals(BYTE));
+	}
+
+	public static boolean isLowLevelType(char first, String s) {
+		return first == '[' || (first == 'L' && s.endsWith(";"));
+	}
 }
--- src/main/java/heapdl/hprof/StackTraces.java
+++ src/main/java/heapdl/hprof/StackTraces.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import static heapdl.core.DumpParsingUtil.*;
+
 public class StackTraces {
 
     private final HashMap<Long, StackTrace> stackTraces = new HashMap<>();
@@ -24,7 +26,17 @@ public class StackTraces {
                     stackFrames = new ArrayList<>();
                     objId = frame[0];
                 }
-                stackFrames.add(new StackFrame(frame[2], frame[3], frame[5], Integer.parseInt(frame[4])));
+                String methodSignature = frame[3];
+                //System.out.println("Method signature: " + frame[3]);
+                String arguments = methodSignature.substring(methodSignature.indexOf("(")+1, methodSignature.lastIndexOf(")"));
+	            String raisedReturnType = raiseTypeId(methodSignature.substring(methodSignature.lastIndexOf(")")+1));
+	            String raisedSignature = raisedReturnType + " " + frame[2] + "(";
+
+	            if (!arguments.isEmpty()) {
+                    raisedSignature += convertArguments(arguments);
+	            }
+	            raisedSignature += ")";
+                stackFrames.add(new StackFrame(frame[2], raisedSignature, raiseTypeId(frame[5]), Integer.parseInt(frame[4])));
             }
         } catch (IOException e) {
             e.printStackTrace();
